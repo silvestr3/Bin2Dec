dacfd08d163366c1af058260a8cee24f82031522
FellipeSantosSilvest <FellipeSantosSilvest@DCIT-12>
2022-01-28 07:50:23 -0300

    Add project files.

---
+++ Bin2Dec/Bin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bin2Dec
+{
+    class Bin
+    {
+        public string Binary { get; set; }
+
+        public Bin(string bin)
+        {
+            this.Binary = bin;
+        }
+
+
+    }
+}
